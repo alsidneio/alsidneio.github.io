a330127a400e702c4a26ff6d925416696d053999
Alsidneio <alsidneio@gmail.com>
2017-05-05 01:09:37 -0700

    all the content is there, now all that is left is styling

--- js/app.js
+++ js/app.js
@@ -1,13 +1,7 @@
 
 $(document).ready(function(){
-  $('#html').circleProgress({
-      value: .8,
-      size: 120,
-      startAngle:0,
-      lineCap:"square",
-      thickness:10,
-      fill: {
-        gradient: ["red", "orange"]
-      }
-    });
+  function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
 });
