edd79129d629dff8f798ecaf49a1f22e65c9f574
Alsidneio <alsidneio@gmail.com>
2017-05-06 00:12:21 -0700

    finishing up some styling

--- js/app.js
+++ js/app.js
@@ -3,5 +3,9 @@ $(document).ready(function(){
   function adjust_textarea(h) {
     h.style.height = "20px";
     h.style.height = (h.scrollHeight)+"px";
+
+
 }
+
+$(" .year p").css("font-weight", "Bold");
 });
