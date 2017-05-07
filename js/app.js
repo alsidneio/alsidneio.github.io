de5e98eb641e88793d82e33bac8fa22413b8aede
Alsidneio <alsidneio@gmail.com>
2017-05-07 10:27:43 -0700

    Edited some styling to create a uniform look

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
   function adjust_textarea(h) {
     h.style.height = "20px";
     h.style.height = (h.scrollHeight)+"px";
-
-
 }
 
+$(".year").click(function(){
+  $("").toggle();
+});
+
 $(" .year p").css("font-weight", "Bold");
+
 });
